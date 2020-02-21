f8371bac250c11271b8a0abe0a84b2f6516c5305
zz897993363 <897993363@qq.com>
2020-02-21 17:04:35 +0800

    删除多余依赖

--- YuGiOh2/ClientApp/src/index.js
+++ YuGiOh2/ClientApp/src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Duel } from './components/Duel';
 import { Home } from './components/Home';
-import { DataBase } from './components/DataBase';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 const SliderComponent = () => (
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path="/duel" component={Duel} />
-        <Route path="/db" component={DataBase} />
     </Switch>
 )
 
@@ -23,6 +19,4 @@ ReactDOM.render(
         <SliderComponent />
     </Router>,
     rootElement);
-
-registerServiceWorker();
 
